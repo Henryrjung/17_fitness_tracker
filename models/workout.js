d9b07737650035ebf5066940b5a3993f386b8776
HenryJ <henryjung667@gmail.com>
2021-01-20 21:18:49 -0600

    updated workout model to include different types of exercises

--- models/workout.js
+++ models/workout.js
@@ -2,4 +2,23 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+            type: String,
+            name: String,
+            time: Number,
+            sets: Number,
+            weight: Number,
+            distance: Number,
+        },
+    ]
+})
+
+const Workout = mongoose.model('Workout', workoutSchema);
+
 module.exports = Workout;
