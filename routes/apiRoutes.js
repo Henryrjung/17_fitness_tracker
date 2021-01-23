9a0deafc01a044d7cdd99b52237111a846ab91b6
HenryJ <henryjung667@gmail.com>
2021-01-23 13:45:41 -0600

    fixes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const db = require("../models");
+const db = require("../models/Workout");
 
 router.get("/workouts", (_req, res) => {
   db.Workout.find({}).then((r) => {
